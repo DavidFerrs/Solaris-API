306220c7654c011abfb4e5192237a966c6a9b73b
David Silva <fernandes.-.david@hotmail.com>
2022-11-19 15:36:07 -0300

    base structure of the project

--- src/main/java/com/api/solaris/SolarisApplication.java
+++ src/main/java/com/api/solaris/SolarisApplication.java
@@ -1,10 +1,23 @@
 package com.api.solaris;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SolarisApplication {
+
+	@Bean
+	public ModelMapper getModelMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration()
+				.setFieldMatchingEnabled(true)
+				.setFieldAccessLevel(Configuration.AccessLevel.PRIVATE);
+
+		return modelMapper;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SolarisApplication.class, args);
